54bbaed74fe99c8d03af78ba59575d4aae71201c
Soyon Lee <slee550@myseneca.ca>
2023-03-29 20:23:35 -0400

    error: ch->wd->ch, ch acts like wd

--- app.js
+++ app.js
@@ -1,30 +1,76 @@
 const form = document.querySelector("#textbox");
 const wSpace = document.querySelector("#withSpace");
 const woSpace = document.querySelector("#woSpace");
+const resetBtn = document.querySelector("#reset");
+const charBtn = document.querySelector("#ch");
+const wordBtn = document.querySelector("#wd");
 
-//refine the logic:
-//without whitespace === with whitespace - num of \s(regex)
-form.addEventListener('input', function(e){
+function countChar(e){
     const input = e.target.value;
-    let arr = [];
+    const blank = /\s/;
+
+    let len = 0;
+    let len2 = 0;
+    let blankCnt = 0;
+    let newlineCnt = 0;
     input.split('').forEach(function(ch){
-        if(ch != ' ') arr.push(ch);
+        if(blank.test(ch)) blankCnt++;
+        if(ch === '\n') newlineCnt++;
     })
 
-    let len = input.length;
-    let len2 = arr.join('').length;
+    len = input.length - newlineCnt;
+    len2 = len - blankCnt + newlineCnt;
 
     wSpace.innerText = `${len}`;
     woSpace.innerText = `${len2}`;
+}
 
-})
+function countWord(e){
+    const input = e.target.value;
+    const blank = /\s/;
+    let len = 0;
+    len = input.split(blank).length - 1;
+    wSpace.innerText = `${len}`;
+}
 
-//when the page loaded or 'character' button clicked
-//h1 = Character counter
-//character button disabled
-//when 'input' event fired, count char
+function charSetup(){
+    console.log("char setup fired");
+    charBtn.disabled = true;
+    wordBtn.disabled = false;
+    document.querySelector("h1").innerHTML=`<span class="material-symbols-outlined">
+    summarize
+</span>Character Counter<span class="material-symbols-outlined">
+    summarize
+</span>`;
+    document.querySelector("#result1").innerText = "With whitespace: ";
+    document.querySelector("#result2").hidden = false;
+    document.querySelector("#result2").innerText = "Without whitespace: ";
+    document.querySelector("#woSpace").hidden = false;
+    form.addEventListener('input', countChar);
+    wordBtn.addEventListener('click', wordSetup);
+}
 
-//when the 'word' button clicked
-//h1 = word counter
-//word button disabled
-//when 'input' event fired, count word
+function wordSetup(){
+    console.log("word setup fired");
+    wordBtn.disabled = true;
+    charBtn.disabled = false;
+    document.querySelector("h1").innerHTML=`<span class="material-symbols-outlined">
+    summarize
+</span>Word Counter<span class="material-symbols-outlined">
+    summarize
+</span>`;
+    document.querySelector("#result1").innerText = "How many words: ";
+    document.querySelector("#result2").hidden = true;
+    document.querySelector("#woSpace").hidden = true;
+    form.addEventListener('input', countWord);
+    charBtn.addEventListener('click', charSetup);
+}
+
+document.addEventListener("DOMContentLoaded", charSetup());
+
+resetBtn.addEventListener('click', function(){
+    form.reset();
+    wSpace.innerText="";
+    woSpace.innerText="";
+});
+
